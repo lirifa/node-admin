4ef42a8b11fa12f3dfcb73fd334e6a1e0f63b1d8
rafa <rafa_im@163.com>
2020-06-18 18:16:17 +0800

    登录成功后跳转到首页

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -83,6 +83,7 @@ export default {
     handleLogin(){
       login(this.loginForm).then(res => {
         console.log(res);
+        this.$router.push({path: '/'})
       })
     },
     handleRegister(){
